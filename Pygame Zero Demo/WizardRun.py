adef165ae72de5d203f933750fe5810e79fb8f71
Peter Ferri <pjferri10@gmail.com>
2022-03-25 01:14:08 -0400

    Animation Updates and a scrolling background

--- Pygame Zero Demo/WizardRun.py
+++ Pygame Zero Demo/WizardRun.py
@@ -13,11 +13,17 @@ wizard = Actor('tile0', (playerCordinates))
 run_images = ['tile0', 'tile1', 'tile2', 'tile3', 'tile4', 'tile5', 'tile6', 'tile7']
 wizard.images = run_images
 
-fireball = Actor('fireball', (1000,160))
+fireball = Actor('fire0', (1000, 140))
+fire_flames = ['fire0','fire1','fire2','fire3','fire4']
+fireball.images = fire_flames
 
 orb = Actor('orb0', (1000, 250)) #250 is where the player is, 160 is just above him
 orb_flare = ['orb1','orb2','orb3','orb4','orb5','orb6','orb7','orb8','orb9','orb10','orb11', 'orb12','orb13']
 orb.images = orb_flare
+
+background = Actor("background3", (320,200))
+background2 = Actor("background3", (960,200))
+background3 = Actor("background3", (1600,200))
 
 WIDTH =  800
 HEIGHT = wizard.height + 200
@@ -28,6 +34,7 @@ BLUE = 0, 0, 255
 WHITE = 255, 255, 255
 BLACK = 0, 0, 0
 LIGHTBLUE = 169,228,239
+DARKGREY = 27,27,27
 
 
 Ground = Rect((-100, 300), (1000, 100)) #It's x,y and then size
@@ -45,11 +52,15 @@ gameOver = False
 def draw():
     screen.clear()
     screen.fill((BLACK))
-    screen.draw.filled_rect(Ground, LIGHTBLUE)
+    background.draw()
+    background2.draw()
+    background3.draw()
+    screen.draw.filled_rect(Ground, DARKGREY)
     fireball.draw()
     wizard.draw()
     orb.draw()
-    screen.draw.rect(wizardCheck,WHITE) #This shows the collision rectangle
+    
+    #screen.draw.rect(wizardCheck,WHITE) #This shows the collision rectangle
     screen.draw.text(str(score), midtop=(400, 0))
     if gameOver:
         screen.draw.text("Game Over", (350, 60))
@@ -57,12 +68,24 @@ def draw():
     
 wizard.fps = 10 #Controls the animation speed, the default is 5
 orb.fps = 10
+fireball.fps = 10
 
 fireballSpeed = 4
 orbSpeed = 3
 
 def update(): 
     global score
+    if gameOver == False:
+        background.x -= 1.75
+        background2.x -= 1.75
+        background3.x -= 1.75
+    if background.x < -320:
+        background.x = 1600
+    if background2.x < -320:
+        background2.x = 1600
+    if background3.x < -320:
+        background3.x = 1600
+
     fireball.x -= fireballSpeed
     if fireball.x < 0 and gameOver == False: #If fireball reaches the end, reset it and increase the score by one
         fireball.x = 1000
@@ -77,6 +100,7 @@ def update():
         clock.schedule(increaseDifficulty, 0.1)
 
     orb.animate()
+    fireball.animate()
     checkGameOver()
 
 def on_key_down(key):
@@ -86,7 +110,7 @@ def on_key_down(key):
         if key == keys.SPACE:
             i = 0
             while i < 100: #The purpose of using a loop here is so that the player gradually moves rather than teleports
-                wizard.image = "tile5"
+                wizard.image = "jump0"
                 wizard.y -= 1
                 wizardCheck.y -= 1
                 i+=1
@@ -100,10 +124,11 @@ def land():
     wizardCheck.y = playerYpos - 40
     jumpCount = 0
 
+#alt fireball.colliderect(wizardCheck)
 def checkGameOver():
     global gameOver
-    if fireball.colliderect(wizardCheck) or orb.circle_collidepoint(70, wizardCheck.x, wizardCheck.y): #70 represents the size of the collision circle, the x and y are 
-        wizard.image = "tile6"
+    if fireball.circle_collidepoint(70, wizardCheck.x, wizardCheck.y) or orb.circle_collidepoint(70, wizardCheck.x, wizardCheck.y): #70 represents the size of the collision circle, the x and y are 
+        wizard.image = "death5"
         gameOver = True
 
 
---
+++ Pygame Zero Demo/castle_indoor_background/README.txt
@@ -0,0 +1,7 @@
+
+Visit my page at
+	https://tucangames.itch.io/
+to check more of my projects!
+
+Follow me on Twitter: @GamesTucan
+https://twitter.com/GamesTucan
